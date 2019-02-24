e057851a5478db5661adbfe385a13fce855f831c
Zishan Ali <zishanali06@gmail.com>
2019-02-24 09:16:18 -0600

    added funtionality to click to see experiences

--- scripts.js
+++ scripts.js
@@ -1,10 +1,14 @@
 let expClick = document.querySelector(".clickToSee");
 
-expClick.addEventListener("click", function(){
+//added funtionality to the Click to See Experiences
+expClick.addEventListener("click", function showCards(){
     let cards = document.querySelectorAll(".card");
+
 
     cards.forEach(function(card){
         card.style.display = "block";
     });
     
+    expClick.classList.remove('clickToSee');
+    expClick.removeEventListener("click", showCards);
 });
